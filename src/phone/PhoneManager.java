9d425b77caf63a86012541b21832cd32d64f5533
Daniel Klemm <daniel.klemm@datameer.com>
2017-01-19 15:53:03 +0100

    Builds now with maven & uses special code formatting

--- src/phone/PhoneManager.java
+++ src/phone/PhoneManager.java
@@ -3,6 +3,9 @@ package phone;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+
+import javaslang.control.Option;
 
 import javax.websocket.Session;
 
@@ -10,27 +13,34 @@ import com.google.common.collect.Sets;
 
 public class PhoneManager {
 
-	private final HashMap<String, HashSet<Session>> _phoneMap;
-	private static final AtomicReference<PhoneManager> _singletonHolder = new AtomicReference<>();
+    private final HashMap<String, HashSet<Session>> _phoneMap;
+    private static final AtomicReference<PhoneManager> _singletonHolder = new AtomicReference<PhoneManager>();
 
-	public static final PhoneManager instance() {
-		if (_singletonHolder.get() == null) {
-			_singletonHolder.set(new PhoneManager());
-		}
-		return _singletonHolder.get();
-	}
+    public static final PhoneManager instance() {
+        if (_singletonHolder.get() == null) {
+            _singletonHolder.set(new PhoneManager());
+        }
+        return _singletonHolder.get();
+    }
 
-	public void registerPhoneNumber(String phoneNumber, Session session) {
-		if (!_phoneMap.keySet().contains(phoneNumber)) {
-			HashSet<Session> sessions = Sets.newHashSet(session);
-			_phoneMap.put(phoneNumber, sessions);
-		} else {
-			_phoneMap.get(phoneNumber).add(session);
-		}
-		System.err.println(_phoneMap.get(phoneNumber));
-	}
+    public void registerPhoneNumber(String phoneNumber, Session session) {
+        if (!_phoneMap.keySet().contains(phoneNumber)) {
+            HashSet<Session> sessions = Sets.newHashSet(session);
+            _phoneMap.put(phoneNumber, sessions);
+        } else {
+            _phoneMap.get(phoneNumber).add(session);
+        }
+        System.err.println(_phoneMap.get(phoneNumber));
+    }
 
-	private PhoneManager() {
-		_phoneMap = new HashMap<String, HashSet<Session>>();
-	}
+    private PhoneManager() {
+        Option.of(1)
+                .map(new Function<Integer, Integer>() {
+                    @Override
+                    public Integer apply(Integer t) {
+                        return t + 1;
+                    }
+                });
+        _phoneMap = new HashMap<String, HashSet<Session>>();
+    }
 }
