9a656edd1b5761b1743c9f5b0295088c63c8dba9
Daniel Klemm <daniel.klemm@datameer.com>
2017-02-01 10:41:49 +0100

    Added Immutable generation

---
+++ src/action/FoobarValue.java
@@ -0,0 +1,17 @@
+package action;
+
+import java.util.List;
+import java.util.Set;
+
+import org.immutables.value.Value;
+
+@Value.Immutable
+public abstract class FoobarValue {
+    public abstract int foo();
+
+    public abstract String bar();
+
+    public abstract List<Integer> business();
+
+    public abstract Set<Long> cruise();
+}
